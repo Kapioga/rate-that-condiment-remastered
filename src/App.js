4f42571f1c1e933fd3f5a5ee23995cd307ae3557
Kapioga <Ldaleyvo@gmail.com>
2024-10-25 11:27:18 -0400

    Updated Website Design XD file

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Is this your site?
         </a>
       </header>
     </div>
